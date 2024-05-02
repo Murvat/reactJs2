6a22cd2f303fad6150aa099555af43217af27754
Murvat <murvat.mustafayev@mail.ru>
2024-05-02 20:47:14 +0100

    .

--- 6sprint/unit-06-hw/src/App.js
+++ 6sprint/unit-06-hw/src/App.js
@@ -4,7 +4,7 @@ import Homework1 from './Homework1';
 function App() {
   return (
     <>
-      <Homework1 />
+      <Homework1 p1={88} />
     </>
   );
 }
--- 6sprint/unit-06-hw/src/Homework1.js
+++ 6sprint/unit-06-hw/src/Homework1.js
@@ -1,19 +1,45 @@
 import React from "react";
 
 class Homework1 extends React.Component {
-    constructor() {
+    constructor(props) {
+        super();
+        this.props = props;
+        this.state = {
+            s1: this.props.p1
+        }
+        this.s2 = 201;
+        console.log(props);
+    }
+    componentDidMount() {
+
+        let a = this.state.s1;
+        a += 5
+        this.setState({ s1: a });
+
+        this.s2 += 5;
 
     }
 
+    buttonHander = () => {
+        let a = this.state.s1;
+        a += 50;
+        this.setState({ s1: a });
+
+        this.s2 += 50;
+    }
 
     render() {
+
         return (
             <>
-                <h1>Hello</h1>
+                <div>{this.state.s1}</div>
+                <div>{this.s2}</div>
+                <div>
+                    <button onClick={this.buttonHander} style={{ width: '60px', height: '30px' }}>Button</button>
+                </div >
             </>
         )
     }
-
-};
+}
 
 export default Homework1;
