a1b1ea45bcbfdec606c55d37dead7b31452dd6b3
Murvat <murvat.mustafayev@mail.ru>
2024-05-06 19:43:30 +0100

    reddux_store

---
+++ 11sprint/unit-11/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App';
+
+
+import { legacy_createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/'
+import initialState from './store/initialState';
+
+const store = legacy_createStore(rootReducer, initialState);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(
+
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </ Provider >
+
+  </React.StrictMode>
+);
---
+++ 11sprint/unit-11/src/store/initialState.js
@@ -0,0 +1,12 @@
+const initialState = {
+    goods: [
+        {
+            id: 'sdsd',
+            title: 'Смартфон Xiaomi Redmi 12 8GB/256GB Sky Blue',
+            image: 'https://strgimgr.umico.az/sized/280/602827-150b86ff7d6f988fad34b30b27f461e2.jpg',
+        }
+    ]
+}
+
+export default initialState
+
